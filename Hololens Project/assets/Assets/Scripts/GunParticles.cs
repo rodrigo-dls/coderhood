ba43130522a5aa5bd0900b73da5604ef9b418b31
rodrigo-dls <rodri.m.dls@gmail.com>
2016-09-04 20:43:51 -0300

    Ordenando el repositorio

---
+++ Hololens Project/assets/Assets/Scripts/GunParticles.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GunParticles : MonoBehaviour {
+
+    public void Hide()
+    {
+        StartCoroutine(WaitForDisable());
+    }
+
+    private IEnumerator WaitForDisable()
+    {
+        yield return new WaitForSeconds(0.5f);
+        gameObject.SetActive(false);
+    }
+}
